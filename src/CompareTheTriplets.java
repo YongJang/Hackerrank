f8c623e1101dc3dfc4ef54d75a88ec1073699b17
YongJang <yj_430@naver.com>
2016-12-17 16:49:19 +0900

    solve Compare-the-triplets

---
+++ src/CompareTheTriplets.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by USER on 2016-12-17.
+ */
+public class CompareTheTriplets {
+    public static void main (String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String aString = in.readLine();
+        String bString = in.readLine();
+        String aSplit[] = aString.split("\\s");
+        String bSplit[] = bString.split("\\s");
+        int sumA, sumB;
+        sumA = sumB = 0;
+
+        for (int i = 0; i < aSplit.length; i++) {
+            if (Integer.parseInt(aSplit[i]) > Integer.parseInt(bSplit[i])) {
+                sumA++;
+            } else if (Integer.parseInt(aSplit[i]) < Integer.parseInt(bSplit[i])) {
+                sumB++;
+            }
+        }
+
+        System.out.printf("%d %d\n", sumA, sumB);
+    }
+}
