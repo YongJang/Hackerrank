948194e6adfe294011eaaa2fb3f888ac59a2b502
YongJang <yj_430@naver.com>
2017-06-12 22:15:21 +0900

    123

--- src/BirthdayCakeCandles.java
+++ src/BirthdayCakeCandles.java
@@ -4,5 +4,6 @@
 public class BirthdayCakeCandles {
     public static void main(String args[]) {
         System.out.println("TEST");
+        System.out.println("why .ignore doesn't work appropriately!!!" );
     }
 }
