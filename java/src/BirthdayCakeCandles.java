d6f029f6c786a4286799a1740e21ebd9569a84ce
YongJang <yj_430@naver.com>
2017-06-13 19:39:52 +0900

    solve easy quiz

--- java/src/BirthdayCakeCandles.java
+++ java/src/BirthdayCakeCandles.java
@@ -1,8 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Administrator on 2017-06-12.
  */
 public class BirthdayCakeCandles {
-    public static void main(String args[]) {
-        System.out.println("test");
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String strs[] = br.readLine().split("\\s+");
+        int candlesHeight[] = new int[strs.length];
+        for (int i = 0; i < candlesHeight.length; i++) {
+            candlesHeight[i] = Integer.parseInt(strs[i]);
+        }
+        int max = candlesHeight[0];
+        int maxCount = 0;
+        for (int i = 0; i < candlesHeight.length; i++) {
+            if (max == candlesHeight[i])
+                maxCount++;
+            if (max < candlesHeight[i]) {
+                max = candlesHeight[i];
+                maxCount = 1;
+            }
+        }
+        System.out.println(maxCount);
     }
 }
