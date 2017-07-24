bc18d089f107c7bce118db46d297a653b65ca8a6
YongJang <jyj3312@gmail.com>
2017-07-24 13:14:04 +0900

    Grading Problem solve
    Grading Problem solve

---
+++ java/src/Grading.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author YongJang
+ * https://www.hackerrank.com/challenges/grading
+ */
+public class Grading {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        int n;
+
+        for (int i = 0; i < t; i++) {
+            n = Integer.parseInt(in.readLine());
+            grading(n);
+        }
+    }
+
+    public static void grading(int n) {
+        int reminder = n % 5;
+        int quotient = n / 5;
+        int result = n;
+
+        if (n > 37) {
+            if (reminder >= 3) {
+                result = (quotient + 1) * 5;
+            }
+        }
+        System.out.println(result);
+    }
+}
