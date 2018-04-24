d43772908f95ea68e9dcb0d2430dfb21340283ef
YongJang <jyj3312@gmail.com>
2018-04-24 15:23:11 +0900

    solve 1 problem with go

---
+++ go/SolveMeFirst.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+/**
+ * Author : YongJang
+ * https://www.hackerrank.com/challenges/solve-me-first/problem
+*/
+
+func solveMeFirst(a uint32, b uint32) uint32 {
+  return (a + b)
+}
+
+func main() {
+  var a, b, res uint32
+  fmt.Scanf("%v\n%v", &a, &b)
+  res = solveMeFirst(a, b)
+  fmt.Println(res)
+}
